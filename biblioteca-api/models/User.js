44795ec318d1637ac051e422e51a8cd5e34f2bfa
AlanaDeFrancaSilva <alana14.franca@gmail.com>
2024-11-04 23:00:12 -0300

    04/11

--- biblioteca-api/models/User.js
+++ biblioteca-api/models/User.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     userType: { type: String, required: true },
     nif: { type: String },
     rm: { type: String },
-    phone: { type: String }
-});
+    phone: { type: String },
+}, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
